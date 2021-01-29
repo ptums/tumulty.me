13cd4bc83f2dd4af430ce2b54735d3b26cdd09e4
ptums <ptumulty923@gmail.com>
2021-01-29 12:03:08 -0500

    integrated sitemap generator on build

--- web/next.config.js
+++ web/next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   images: {
     domains: [`cdn.sanity.io`],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./utils/generate-sitemap')
+    }
+
+    return config
+  }
 }
---
+++ web/utils/generate-sitemap.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const globby = require('globby')
+
+async function generateSiteMap() {
+  const pages = await globby([
+    '.next/server/pages/post/*.html',
+    'pages/**/*.js',
+    '!pages/_*.js',
+    '!pages/**/[slug].js',
+    '!pages/api',
+    'posts/*.md'
+  ]);
+  console.log('pages');
+  console.log(pages);
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+          ${pages
+            .map(page => {
+              const path = page
+                .replace('pages', '')
+                .replace('.js', '')
+                .replace('.md', '')
+                .replace('.html', '')
+                .replace('.next/server/','')
+              const route = path === '/index' ? '' : path
+              return `
+                      <url>
+                          <loc>${`https://tumulty.me${route}`}</loc>
+                      </url>
+                  `
+            })
+            .join('')}
+      </urlset>`
+
+  fs.writeFileSync('public/sitemap.xml', sitemap)
+}
+
+generateSiteMap()
